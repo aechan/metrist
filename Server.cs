32ea02ab73b0702c69f79b06fc56fb3e63924875
Ubuntu <ubuntu@ip-172-31-21-236.us-west-2.compute.internal>
2018-02-19 04:20:12 +0000

    couple edits

--- Server.cs
+++ Server.cs
@@ -16,7 +16,7 @@ public class Server
         .UseWebRoot(Path.Combine(Directory.GetCurrentDirectory(), "public"))
         .UseKestrel()
         .UseStartup<Startup>()
-        .UseUrls("http://localhost:80")
+        .UseUrls("http://0.0.0.0:80")
         .Build();
 
         host.Run();
